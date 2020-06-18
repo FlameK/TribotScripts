24962d4e2fe1c42b915456bf7016710be9cab86d
FlameK <elliottbarnes1@yahoo.co.uk>
2020-06-18 11:36:17 +0200

    Update

--- src/scripts/api/BankingHelper.java
+++ src/scripts/api/BankingHelper.java
@@ -9,6 +9,7 @@ import org.tribot.api2007.*;
 import org.tribot.api2007.types.RSInterface;
 import org.tribot.api2007.types.RSInterfaceChild;
 import org.tribot.api2007.types.RSItem;
+import org.tribot.api2007.types.RSVarBit;
 import scripts.api.antiban.Antiban;
 import scripts.dax_api.api_lib.DaxWalker;
 import scripts.dax_api.api_lib.models.RunescapeBank;
@@ -18,14 +19,10 @@ import java.awt.event.KeyEvent;
 public class BankingHelper {
 
     public static String bankHelperText = "[Bank Helper]: ";
-    public static int regularSleep = 850;
-    public static int shortSleep = 750;
-    public static int longSleep = 1050;
-    private static final int BANK_MASTER_ID = 12,
-            WITHDRAW_AS_ITEM_ID = 21,
-            WITHDRAW_AS_NOTED_ID = 23,
-            WITHDRAW_AS_ITEM_BACKGROUND_ID = 20;
+    public static int regularSleep = 850, shortSleep = 750, longSleep = 1050;
+    public static int regularSD = 75, shortSD = 50, longSD = 100;
 
+    private static final int BANK_MASTER_ID = 12, WITHDRAW_AS_ITEM_ID = 21, WITHDRAW_AS_NOTED_ID = 23, WITHDRAW_AS_ITEM_BACKGROUND_ID = 20;
 
     private static BankingHelper bankingHelper;
 
@@ -39,7 +36,7 @@ public class BankingHelper {
             Keyboard.sendPress(KeyEvent.CHAR_UNDEFINED, KeyEvent.VK_ESCAPE);
             General.println(bankHelperText + "Attempting to close bank with escape key.");
             Timing.waitCondition(() -> {
-                General.sleep(General.randomSD(regularSleep, 75));
+                General.sleep(General.randomSD(regularSleep, regularSD));
                 return !Banking.isBankScreenOpen();
             }, General.random(5000, 6000));
         }
@@ -82,20 +79,62 @@ public class BankingHelper {
         return false;
     }
 
-    public static boolean equipItemWithBankOpen(String item) {
+    public static boolean equipItemWithBankOpen(String itemName) {
 
-        RSItem[] i = Inventory.find(item);
+        RSItem[] i = Inventory.find(itemName);
         if (Banking.isBankScreenOpen()) {
 
             if (i != null && i.length > 0) {
+                General.println(bankHelperText + "Attempting to equip: " + itemName);
                 if (i[0].click("Wear")) {
                     Antiban.waitItemInteractionDelay();
                     Timing.waitCondition(() -> {
-                        General.sleep(General.randomSD(750, 75));
-                        return Inventory.find(item).length == 0;
+                        General.sleep(General.randomSD(regularSleep, regularSD));
+                        return Inventory.find(itemName).length == 0;
                     }, General.random(2000, 3000));
                 }
-                General.println("BANKING: Equipped item " + item);
+                General.println(bankHelperText + "Equipped item " + itemName);
+            }
+        }
+        return false;
+    }
+
+    public static boolean drinkStaminaPotionWithBankOpenAndDeposit(String itemName) {
+        RSItem[] i = Inventory.find(itemName);
+        int staminaVarbit = 0;
+        int staminaPotionInEffect = 1;
+        int staminaPotionNoEffect = 0;
+        int amount = Inventory.getCount(itemName);
+
+        if (Banking.isBankScreenOpen() && Game.getVarBit(staminaVarbit) == staminaPotionNoEffect) {
+            if (i[0].click("Drink")) {
+                Antiban.waitItemInteractionDelay();
+                Timing.waitCondition(() -> {
+                    General.sleep(General.randomSD(regularSleep, regularSD));
+                    return Game.getVarBit(staminaVarbit) == staminaPotionInEffect;
+                }, General.random(3000, 4000));
+            }
+        }
+        else if (Game.getVarBit(staminaVarbit) == staminaPotionInEffect) {
+            General.println(bankHelperText + "Stamina potion is already activated, attempting to deposit item");
+        }
+        depositItem(amount, itemName);
+        return false;
+    }
+
+    public static boolean depositItem(int amount, String itemName) {
+        RSItem[] i = Inventory.find(itemName);
+
+        if (Banking.isBankScreenOpen() && Banking.isBankLoaded()) {
+            General.println(bankHelperText + "Bank is open, attempting to deposit " + amount + " " + itemName);
+            if (Inventory.find(itemName).length > 0) {
+                Banking.deposit(amount, itemName);
+                Antiban.waitItemInteractionDelay();
+                Timing.waitCondition(() -> {
+                    General.sleep(General.randomSD(regularSleep, regularSD));
+                    return Inventory.find(itemName).length == 0;
+                }, General.random(3000, 4000));
+                General.println(bankHelperText + "Successfully deposited " + amount + " " + itemName);
             }
         }
         return false;
